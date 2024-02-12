a3a73f2b5a018c5eb4bcd9b5663f55d6dd23fcdd
arpitaM130 <149518540+arpitaM130@users.noreply.github.com>
2024-02-12 13:54:51 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+// 1. Import Express
+import express from 'express';
+import bodyParser from 'body-parser';
+import ProductRouter from './src/features/product/product.routes.js';
+import UserRouter from './src/features/user/user.routes.js';
+import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
+import jwtAuth from './src/middlewares/jwt.middleware.js';
+
+// 2. Create Server
+const server = express();
+
+server.use(bodyParser.json());
+
+// for all requests related to product , redirect to product routes.
+server.use('/api/products', jwtAuth ,ProductRouter); 
+server.use('/api/users',UserRouter); 
+
+// 3. Default request handler
+server.get("/", (req, res) => {
+    res.send("Welcome to Ecommerce APIs");
+})
+
+// 4. Specify port 
+server.listen(3200, ()=> {
+    console.log('Server is running on Port: ', 3200);
+});
---
+++ src/features/product/product.routes.js
@@ -0,0 +1,17 @@
+// 1. Import express.
+import express from 'express';
+import ProductController from './product.controller.js';
+import { upload } from '../../middlewares/fileupload.middleware.js';
+
+// 2. Initialize Express router.
+const router = express.Router();
+
+const productController = new ProductController();
+
+// All the paths to controller methods(/api/products).
+router.get('/filter', productController.filterProducts);
+router.get('/', productController.getAllProducts);
+router.post('/', upload.single('imageUrl'), productController.addProduct);
+router.get('/:id', productController.getOneProduct);
+
+export default router;
